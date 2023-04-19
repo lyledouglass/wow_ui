60d0f72ef0d4c449405028ec14c310fc8c2c2405
Lyle Douglass <lyle.douglass@gmail.com>
2023-04-19 18:39:40 -0400

    init commit

---
+++ OPie.lua
@@ -0,0 +1,842 @@
+
+OneRing_Config = nil
+OPie_SavedData = {
+	["CharProfiles"] = {
+		["Stormrage-Oxylliah"] = "default",
+	},
+	["_GameVersion"] = "10.0.7",
+	["_OPieVersion"] = "Yuzu 8a (4.114)",
+	["ProfileStorage"] = {
+		["default"] = {
+			["RingOptions"] = {
+				["Monk#SelectedSliceBind"] = "",
+			},
+			["Bindings"] = {
+				["emonHunter"] = "BUTTON4",
+				["SpecMenu"] = false,
+				["Rogue"] = "BUTTON4",
+				["DruidFeral"] = false,
+				["WorldMarkers"] = "ALT-X",
+				["Hunter"] = "BUTTON4",
+				["DK"] = "BUTTON4",
+				["Warrior"] = "BUTTON4",
+				["Paladin"] = "BUTTON4",
+				["Shaman"] = "BUTTON4",
+				["Food"] = "F2",
+				["DruidShift"] = "BUTTON5",
+				["HunterAspects"] = false,
+				["Evoker"] = "BUTTON4",
+				["Monk"] = "BUTTON4",
+				["MageCombat"] = false,
+				["Priest"] = "BUTTON4",
+				["Mage"] = "BUTTON4",
+				["DruidUtility"] = false,
+				["Druid"] = "BUTTON4",
+			},
+			["RotationTokens"] = {
+				["ABue5kT2=md"] = "ABuezb=uPv2",
+				["ABuezKAGw6a"] = "ABuezb=uPvU",
+				["ABuexngjkxu"] = "ABue5YItyvt",
+				["ABuezZiDpfs"] = "ABuebn3nKhe",
+				["ABuexngjkxy"] = "ABuebn3nKhe",
+				["ABuex8zGjEs"] = "ABuebn3nKhe",
+				["ABuevxcn2Zi"] = "ABuebvpwfAw",
+				["ABue5g69O6i"] = "ABuebvpwfAw",
+				["ABuez9kw2io"] = "ABuezb=uPv2",
+				["ABuex8zGjEo"] = "ABuevY=Z9Zw",
+				["ABuebb0iOz6"] = "ABuebb0iOzl",
+				["ABuex8zGjEa"] = "ABue5YItyvy",
+				["ABuevM=uk0r4"] = "ABuebfr5ysw",
+				["ABue5g69O6u"] = "ABuebR2660w",
+				["ABuezOwDdFz"] = "ABuebUNutTw",
+				["ABuexKReFyw"] = "ABue5YItyvy",
+				["ABuezKAGw6s"] = "ABuezb=uPv2",
+				["ABuebn3d8Ge"] = "ABue5YItyv1",
+				["ABuezZiDpfa"] = "ABuebR2660w",
+				["ABuez9kw2ip"] = "ABuevY=Z9Zw",
+			},
+		},
+	},
+	["PersistentStorage"] = {
+		["RingKeeper"] = {
+			["emonHunter"] = {
+				{
+					"specset", -- [1]
+					1, -- [2]
+					["sliceToken"] = "ABuez9kw2i2",
+				}, -- [1]
+				{
+					"specset", -- [1]
+					2, -- [2]
+					["sliceToken"] = "ABuez9kw2ii",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuez9kw2ip",
+				}, -- [3]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuez9kw2io",
+				}, -- [4]
+				["save"] = true,
+				["name"] = "Demon Hunter",
+				["limit"] = "DEMONHUNTER",
+			},
+			["SpecMenu"] = {
+				{
+					["sliceToken"] = "OPCTAc",
+					["id"] = "/cast {{spell:50977}}; {{spell:193753}}; {{spell:126892}}; {{spell:193759}}",
+				}, -- [1]
+				{
+					"specset", -- [1]
+					1, -- [2]
+					["sliceToken"] = "ABuekUDLh6u",
+				}, -- [2]
+				{
+					"specset", -- [1]
+					3, -- [2]
+					["sliceToken"] = "ABuekUDLh6y",
+				}, -- [3]
+				{
+					"specset", -- [1]
+					2, -- [2]
+					["sliceToken"] = "ABuekUDLh6t",
+				}, -- [4]
+				{
+					"specset", -- [1]
+					4, -- [2]
+					["sliceToken"] = "ABuel1cwJRe",
+				}, -- [5]
+				{
+					"item", -- [1]
+					110560, -- [2]
+					["sliceToken"] = "OPCTAg",
+				}, -- [6]
+				{
+					"item", -- [1]
+					140192, -- [2]
+					["sliceToken"] = "OPCTAd",
+				}, -- [7]
+				{
+					["id"] = 556,
+					["sliceToken"] = "OPCTAa",
+				}, -- [8]
+				{
+					"toy", -- [1]
+					163045, -- [2]
+					["show"] = "[coven:kyrian][coven:venthyr]",
+					["sliceToken"] = "ABuelS0INre",
+				}, -- [9]
+				{
+					"toy", -- [1]
+					182773, -- [2]
+					["sliceToken"] = "ABuelFklMkr",
+					["show"] = "[coven:necro]",
+				}, -- [10]
+				{
+					"toy", -- [1]
+					180290, -- [2]
+					["sliceToken"] = "ABuelCu2U0t",
+					["show"] = "[coven:fae]",
+				}, -- [11]
+				{
+					"toy", -- [1]
+					172924, -- [2]
+					["show"] = "[engineering]",
+					["sliceToken"] = "ABuelSUb3ie",
+				}, -- [12]
+				{
+					"mount", -- [1]
+					460, -- [2]
+					["sliceToken"] = "ABuejs7kR0a",
+				}, -- [13]
+				{
+					"mount", -- [1]
+					522, -- [2]
+					["sliceToken"] = "ABuekU254Le",
+					["show"] = "[herbalism]",
+				}, -- [14]
+				{
+					["sliceToken"] = "ABuekBV4bQr",
+					["id"] = 150544,
+				}, -- [15]
+				["name"] = "Specializations and Travel",
+				["save"] = true,
+				["hotkey"] = "RIGHT",
+			},
+			["Rogue"] = {
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABue5Ug9ZIa",
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABue5Ug9ZIp",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABue5Ug9ZIo",
+				}, -- [3]
+				["save"] = true,
+				["limit"] = "ROGUE",
+				["name"] = "Rogue",
+			},
+			["DruidFeral"] = {
+				{
+					["id"] = 106951,
+					["sliceToken"] = "OPCDFk",
+				}, -- [1]
+				{
+					["id"] = "/cast [noform:bear] {{spell:5217}}; {{spell:22842}}",
+					["sliceToken"] = "OPCDFe",
+				}, -- [2]
+				{
+					["id"] = "/cast [mod][nospec:2/3] {{spell:1850}}; [form:bear] {{spell:77761}}; {{spell:77764}}",
+					["sliceToken"] = "OPCDFr",
+				}, -- [3]
+				{
+					["id"] = 106839,
+					["sliceToken"] = "OPCDFs",
+				}, -- [4]
+				{
+					["id"] = 22812,
+					["sliceToken"] = "OPCDFb",
+				}, -- [5]
+				{
+					["id"] = 61336,
+					["sliceToken"] = "OPCDFi",
+				}, -- [6]
+				{
+					["id"] = 102401,
+					["sliceToken"] = "OPCDFc",
+				}, -- [7]
+				{
+					["id"] = "/cast {{spell:102543/102558}}",
+					["sliceToken"] = "OPCDFn",
+				}, -- [8]
+				{
+					["show"] = "[spec:102/104/105] hide;",
+					["sliceToken"] = "OPCDFh",
+					["id"] = "/cast [nomod,@player][mod,@none] {{spell:8936}}",
+				}, -- [9]
+				["name"] = "Feral",
+				["save"] = true,
+				["hotkey"] = "[form:bear/cat] BUTTON5; ALT-BUTTON5",
+				["limit"] = "DRUID",
+			},
+			["OPieDeletedRings"] = {
+				["DKCombat"] = true,
+				["PaladinAuras"] = true,
+				["WarlockCombat"] = true,
+				["PaladinTools"] = true,
+			},
+			["OPieFlagStore"] = {
+				["StoreVersion"] = 2,
+			},
+			["WarlockLTS"] = {
+				{
+					"ring", -- [1]
+					"WarlockDemons", -- [2]
+					["sliceToken"] = "OPCLSd",
+				}, -- [1]
+				{
+					["id"] = "/cast [mod:alt] {{spell:20707}}; [group,nomod][nogroup,mod] {{spell:29893}}; {{spell:6201}}",
+					["sliceToken"] = "OPCLSh",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABue5YItyv2",
+				}, -- [3]
+				{
+					["id"] = "/cast [group,nomod][nogroup,mod] {{spell:48018}}; {{spell:111771}}",
+					["sliceToken"] = "ABuezbvmzK2",
+				}, -- [4]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuezb=uPvD",
+				}, -- [5]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuezb=uPvp",
+				}, -- [6]
+				["name"] = "Warlock General",
+				["save"] = true,
+				["hotkey"] = "BUTTON4",
+				["limit"] = "WARLOCK",
+			},
+			["Hunter"] = {
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuevawcpay",
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABuevawcpat",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuevawcpa1",
+				}, -- [3]
+				["save"] = true,
+				["name"] = "Hunter",
+				["limit"] = "Oxylea-Stormrage",
+			},
+			["Evoker"] = {
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABuevxcn2Z2",
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuevxcn2Zu",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuevxcn2Zi",
+				}, -- [3]
+				["name"] = "Evoker",
+				["save"] = true,
+				["hotkey"] = "BUTTON4",
+				["limit"] = "EVOKER",
+			},
+			["DK"] = {
+				{
+					"specset", -- [1]
+					1, -- [2]
+					["sliceToken"] = "ABuezKAGw6f",
+				}, -- [1]
+				{
+					"specset", -- [1]
+					2, -- [2]
+					["sliceToken"] = "ABuezKAGw6d",
+				}, -- [2]
+				{
+					"specset", -- [1]
+					3, -- [2]
+					["sliceToken"] = "ABuezKAGw63",
+				}, -- [3]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuezKAGw6a",
+				}, -- [4]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuezKAGw6s",
+				}, -- [5]
+				["save"] = true,
+				["name"] = "DK",
+				["limit"] = "DEATHKNIGHT",
+			},
+			["Warrior"] = {
+				{
+					"specset", -- [1]
+					1, -- [2]
+					["sliceToken"] = "ABue5vV5XHl",
+				}, -- [1]
+				{
+					"specset", -- [1]
+					2, -- [2]
+					["sliceToken"] = "ABue5vV5XHk",
+				}, -- [2]
+				{
+					"specset", -- [1]
+					3, -- [2]
+					["sliceToken"] = "ABue5vV5XHj",
+				}, -- [3]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABue5vV5XH4",
+				}, -- [4]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABue5vV5XHh",
+				}, -- [5]
+				["save"] = true,
+				["limit"] = "Nollywar-Stormrage",
+				["name"] = "Warrior",
+			},
+			["Specs"] = {
+				{
+					"specset", -- [1]
+					1, -- [2]
+					["sliceToken"] = "ABue5YItyvy",
+				}, -- [1]
+				{
+					"specset", -- [1]
+					2, -- [2]
+					["sliceToken"] = "ABue5YItyvt",
+				}, -- [2]
+				{
+					"specset", -- [1]
+					3, -- [2]
+					["sliceToken"] = "ABue5YItyv1",
+				}, -- [3]
+				["name"] = "Specs",
+				["save"] = true,
+			},
+			["Shaman"] = {
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABuebn3d8Ge",
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABue5g69O6u",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABue5g69O6i",
+				}, -- [3]
+				["save"] = true,
+				["limit"] = "SHAMAN",
+				["name"] = "Shaman",
+			},
+			["Monk"] = {
+				{
+					["sliceToken"] = "ABuezFeL1K1",
+					["id"] = 101643,
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"Mounts2", -- [2]
+					["sliceToken"] = "ABuezOwDdF6",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABuexngjkxu",
+				}, -- [3]
+				{
+					"ring", -- [1]
+					"Portals", -- [2]
+					["sliceToken"] = "ABuevM=uk0r4",
+				}, -- [4]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuezOwDdFz",
+				}, -- [5]
+				{
+					"ring", -- [1]
+					"Wormholes", -- [2]
+					["sliceToken"] = "ABuebb0iOz6",
+				}, -- [6]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuexngjkxy",
+				}, -- [7]
+				["quarantineBind"] = "RIGHT",
+				["name"] = "Monk",
+				["save"] = true,
+				["limit"] = "MONK",
+			},
+			["Wormholes"] = {
+				{
+					"toy", -- [1]
+					112059, -- [2]
+					["sliceToken"] = "ABuebb0iOzb",
+				}, -- [1]
+				{
+					"toy", -- [1]
+					168807, -- [2]
+					["sliceToken"] = "ABuebb0iOzv",
+				}, -- [2]
+				{
+					"toy", -- [1]
+					87215, -- [2]
+					["sliceToken"] = "ABuebb0iOzc",
+				}, -- [3]
+				{
+					"toy", -- [1]
+					172924, -- [2]
+					["sliceToken"] = "ABuebb0iOzx",
+				}, -- [4]
+				{
+					"toy", -- [1]
+					168808, -- [2]
+					["sliceToken"] = "ABuebb0iOz5",
+				}, -- [5]
+				{
+					"toy", -- [1]
+					48933, -- [2]
+					["sliceToken"] = "ABuebb0iOzz",
+				}, -- [6]
+				{
+					"toy", -- [1]
+					198156, -- [2]
+					["sliceToken"] = "ABuebb0iOzl",
+				}, -- [7]
+				["name"] = "Wormholes",
+				["save"] = true,
+				["limit"] = "Nollychi-Stormrage",
+			},
+			["DruidShift"] = {
+				{
+					["show"] = "[known:783]",
+					["sliceToken"] = "OPCDSf",
+					["id"] = "/cancelform [noflyable,noform:moonkin]\n/cast [flyable,outdoors,nocombat,noswimming,nomod][flying] {{spell:783}}; [outpost:corral,nomod,nospec:103/104] {{spell:161691}}; [swimming,nomod][flyable,nomod][flying] {{spell:783}}; [outdoors] {{spell:783}}",
+				}, -- [1]
+				{
+					["sliceToken"] = "OPCDSk",
+					["c"] = "c74cff",
+					["id"] = 24858,
+				}, -- [2]
+				{
+					["sliceToken"] = "OPCDSc",
+					["c"] = "fff04d",
+					["id"] = 768,
+				}, -- [3]
+				{
+					["sliceToken"] = "OPCDSb",
+					["c"] = "ff0000",
+					["id"] = 5487,
+				}, -- [4]
+				["name"] = "Shapeshifts",
+				["save"] = true,
+				["hotkey"] = "BUTTON4",
+				["limit"] = "DRUID",
+			},
+			["Travel"] = {
+				{
+					["sliceToken"] = "ABuezb=uPv9",
+					["id"] = "/cast {{spell:50977}}; {{spell:193753}}; {{spell:126892}}; {{spell:193759}}",
+				}, -- [1]
+				{
+					"item", -- [1]
+					110560, -- [2]
+					["sliceToken"] = "ABuezb=uPv8",
+				}, -- [2]
+				{
+					"item", -- [1]
+					140192, -- [2]
+					["sliceToken"] = "ABuezb=uPvU",
+				}, -- [3]
+				{
+					["id"] = 556,
+					["sliceToken"] = "ABuezb=uPvY",
+				}, -- [4]
+				{
+					"toy", -- [1]
+					165802, -- [2]
+					["sliceToken"] = "ABuebUNutTw",
+				}, -- [5]
+				["quarantineBind"] = "RIGHT",
+				["name"] = "Travel",
+				["save"] = true,
+			},
+			["Portals"] = {
+				{
+					["sliceToken"] = "ABuevM=uk0rh",
+					["id"] = 393766,
+				}, -- [1]
+				{
+					["sliceToken"] = "ABuebfr5ysw",
+					["id"] = 131204,
+				}, -- [2]
+				{
+					["sliceToken"] = "ABuevM=uk0rg",
+					["id"] = 159900,
+				}, -- [3]
+				{
+					["id"] = 393764,
+					["sliceToken"] = "ABuevM=uk0rf",
+				}, -- [4]
+				{
+					["id"] = 354463,
+					["sliceToken"] = "ABuevM=uk0rd",
+				}, -- [5]
+				{
+					["sliceToken"] = "ABuebyPKzww",
+					["id"] = 393262,
+				}, -- [6]
+				{
+					["id"] = 354465,
+					["sliceToken"] = "ABuevM=uk0r3",
+				}, -- [7]
+				{
+					["id"] = 354467,
+					["sliceToken"] = "ABuevM=uk0rs",
+				}, -- [8]
+				{
+					["id"] = 354469,
+					["sliceToken"] = "ABuevM=uk0ra",
+				}, -- [9]
+				{
+					["id"] = 159897,
+					["sliceToken"] = "ABuevM=uk0rp",
+				}, -- [10]
+				{
+					["id"] = 159901,
+					["sliceToken"] = "ABuevM=uk0ro",
+				}, -- [11]
+				{
+					["id"] = 159902,
+					["sliceToken"] = "ABuevM=uk0r2",
+				}, -- [12]
+				{
+					["sliceToken"] = "ABuebEKeyaw",
+					["id"] = 393256,
+				}, -- [13]
+				{
+					["sliceToken"] = "ABuevM=uk0ri",
+					["id"] = 367416,
+				}, -- [14]
+				{
+					["id"] = 373262,
+					["sliceToken"] = "ABuevM=uk0ru",
+				}, -- [15]
+				{
+					["id"] = 354462,
+					["sliceToken"] = "ABuevM=uk0ry",
+				}, -- [16]
+				{
+					["id"] = 354464,
+					["sliceToken"] = "ABuevM=uk0rt",
+				}, -- [17]
+				{
+					["id"] = 354466,
+					["sliceToken"] = "ABuevM=uk0r1",
+				}, -- [18]
+				{
+					["id"] = 354468,
+					["sliceToken"] = "ABuevM=uk0rr",
+				}, -- [19]
+				{
+					["id"] = 159895,
+					["sliceToken"] = "ABuevM=uk0re",
+				}, -- [20]
+				{
+					["id"] = 159898,
+					["sliceToken"] = "ABuevM=uk0rw",
+				}, -- [21]
+				{
+					["sliceToken"] = "ABuevM=uk0rq",
+					["id"] = 159899,
+				}, -- [22]
+				{
+					["id"] = 159896,
+					["sliceToken"] = "ABuevM=uk0eM",
+				}, -- [23]
+				{
+					["sliceToken"] = "ABuebW1uo/w",
+					["id"] = 393273,
+				}, -- [24]
+				["name"] = "Portals",
+				["save"] = true,
+				["limit"] = "Nollychi-Stormrage",
+			},
+			["Mounts"] = {
+				{
+					["sliceToken"] = "ABuebvpwfAw",
+					["id"] = 150544,
+				}, -- [1]
+				{
+					"mount", -- [1]
+					460, -- [2]
+					["sliceToken"] = "ABuezb=uPv2",
+				}, -- [2]
+				{
+					"mount", -- [1]
+					522, -- [2]
+					["sliceToken"] = "ABuezb=uPvi",
+					["show"] = "[herbalism]",
+				}, -- [3]
+				{
+					"mount", -- [1]
+					407, -- [2]
+					["sliceToken"] = "ABuecwtMbpw",
+				}, -- [4]
+				{
+					"mount", -- [1]
+					1591, -- [2]
+					["sliceToken"] = "ABuebn3nKhe",
+				}, -- [5]
+				["name"] = "Mounts",
+				["save"] = true,
+			},
+			["Druid"] = {
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABuex8zGjEa",
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"DruidShift", -- [2]
+					["sliceToken"] = "ABuex8zGjEp",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuex8zGjEo",
+				}, -- [3]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuex8zGjEs",
+				}, -- [4]
+				["save"] = true,
+				["limit"] = "DRUID",
+				["name"] = "Druid",
+			},
+			["Paladin"] = {
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABuexKReFyw",
+				}, -- [1]
+				{
+					["id"] = 212056,
+					["sliceToken"] = "ABuecksHm91",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABuezZiDpfa",
+				}, -- [3]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABuezZiDpfs",
+				}, -- [4]
+				["save"] = true,
+				["limit"] = "PALADIN",
+				["name"] = "Paladin",
+			},
+			["WarlockDemons"] = {
+				{
+					["id"] = 30146,
+					["sliceToken"] = "OPCLDf",
+				}, -- [1]
+				{
+					["id"] = 697,
+					["sliceToken"] = "OPCLDv",
+				}, -- [2]
+				{
+					["id"] = 688,
+					["sliceToken"] = "OPCLDi",
+				}, -- [3]
+				{
+					["id"] = 366222,
+					["sliceToken"] = "ABuexhnQJEw",
+				}, -- [4]
+				{
+					["id"] = 691,
+					["sliceToken"] = "OPCLDh",
+				}, -- [5]
+				["name"] = "Demons",
+				["limit"] = "WARLOCK",
+				["save"] = true,
+				["internal"] = true,
+			},
+			["Priest"] = {
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABuebs0k5/t",
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABue5kT2=mf",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABue5kT2=md",
+				}, -- [3]
+				["save"] = true,
+				["limit"] = "PRIEST",
+				["name"] = "Priest",
+			},
+			["Food"] = {
+				{
+					"item", -- [1]
+					163222, -- [2]
+					["sliceToken"] = "ABuejyX/eaa",
+				}, -- [1]
+				{
+					"item", -- [1]
+					168310, -- [2]
+					["sliceToken"] = "ABuejyX/eap",
+				}, -- [2]
+				["limit"] = "Oxyahna-Stormrage",
+				["save"] = true,
+				["hotkey"] = "F2",
+				["name"] = "Food",
+			},
+			["Mage"] = {
+				{
+					["id"] = 190336,
+					["sliceToken"] = "ABue5OOZzUr",
+				}, -- [1]
+				{
+					"ring", -- [1]
+					"Specs", -- [2]
+					["sliceToken"] = "ABue5OOZzUe",
+				}, -- [2]
+				{
+					"ring", -- [1]
+					"MageTravel", -- [2]
+					["sliceToken"] = "ABue5zkQuAr",
+				}, -- [3]
+				{
+					"ring", -- [1]
+					"Travel", -- [2]
+					["sliceToken"] = "ABue5kTql=a",
+				}, -- [4]
+				{
+					"ring", -- [1]
+					"Mounts", -- [2]
+					["sliceToken"] = "ABue5kTql=p",
+				}, -- [5]
+				["save"] = true,
+				["limit"] = "MAGE",
+				["name"] = "Mage",
+			},
+			["Mounts2"] = {
+				{
+					["id"] = 150544,
+					["sliceToken"] = "ABuezOwDdFc",
+				}, -- [1]
+				{
+					"mount", -- [1]
+					460, -- [2]
+					["sliceToken"] = "ABuezOwDdFx",
+				}, -- [2]
+				{
+					"mount", -- [1]
+					522, -- [2]
+					["show"] = "[herbalism]",
+					["sliceToken"] = "ABuezOwDdFb",
+				}, -- [3]
+				["save"] = true,
+				["name"] = "Mounts",
+				["limit"] = "Nollymonk-Stormrage",
+			},
+		},
+	},
+	["_GameLocale"] = "enUS",
+}
